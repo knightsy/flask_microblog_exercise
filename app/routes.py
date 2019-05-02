262786deb9f2a8108869d6ad5ddb2b435557fe7c
Peter Knight <peter.knight41@gmail.com>
2019-05-02 18:42:23 +0800

    User profile view function

--- app/routes.py
+++ app/routes.py
@@ -10,6 +10,17 @@ from app import db
 from app.forms import RegistrationForm
 
 
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
